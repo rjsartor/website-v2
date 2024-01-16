354bc44c38efa8826e4a04b1586bf7a208dff240
RJ <randal.sartor@gmail.com>
2024-01-15 23:13:13 -0500

    feat: adds hover background to experience; fix MealCraft link

--- src/app/components/experience.tsx
+++ src/app/components/experience.tsx
@@ -11,29 +11,31 @@ type ListItemProps = {
 const ListItem: React.FC<ListItemProps> = ({ experience }) => {
   const { date, title, companyUrl, description, skills, company, achievements } = experience;
    return (
-    <article className="flex flex-col md:flex-row mb-6 gap-4">
-      <div className="md:w-1/4 mb-4 md:mb-0 pt-1">
-        <p className="text-md font-medium secondary-text">{date}</p>
-      </div>
-      <div className="md:w-3/4">
-        <h3 className="text-xl font-bold text-white-800 mb-2">
-          {title} <span className='tertiary-text'>at</span><br></br> 
-          <a className='link-hover' href={companyUrl} target='_blank'> {company} <FaExternalLinkAlt className="inline-block ml-2" /></a>
-        </h3>
-        <h4 className="tertiary-text text-xl mb-2">{description}</h4>
-        <ul style={{ listStyleType: 'disc' }} className='mb-4 secondary-text'>
-          {achievements.map((p, i) => <li key={i}>{p}</li>)}
-          
-        </ul>
-        <div className="flex flex-wrap mt-2">
-          {skills.map((skill) => (
-            <span key={skill} className="bg-teal-700 text-teal-200 rounded-full px-3 py-1 text-xs font-semibold mr-2 mb-2">
-              {skill}
-            </span>
-          ))}
+    <a href={companyUrl} target="_blank" rel="noopener noreferrer" className="group block mb-4">
+      <div className="group flex flex-row items-start rounded-lg overflow-hidden border border-transparent transition duration-300 ease-in-out hover:bg-gray-800 hover:border-gray-700">
+        <div className="w-1/4 p-4">
+          <p className="text-lg font-medium secondary-text">{date}</p>
+        </div>
+        <div className="flex flex-col p-4 w-3/4">
+          <h3 className="text-xl font-bold mb-2">
+            {title} <span className='tertiary-text'>at</span><br></br> 
+            <a className='link-hover' href={companyUrl} target='_blank'>{company}</a>
+          </h3>
+          <h4 className="tertiary-text text-xl mb-2">{description}</h4>
+          <ul style={{ listStyleType: 'disc' }} className='mb-4 secondary-text'>
+            {achievements.map((p, i) => <li key={i}>{p}</li>)}
+            
+          </ul>
+          <div className="flex flex-wrap mt-2">
+            {skills.map((skill) => (
+              <span key={skill} className="bg-teal-700 text-teal-200 rounded-full px-3 py-1 text-xs font-semibold mr-2 mb-2">
+                {skill}
+              </span>
+            ))}
+          </div>
         </div>
       </div>
-    </article>
+    </a>
   );
 };
 
@@ -41,23 +43,14 @@ const Experience = () => {
   return (
     <section>
       <h1 className="text-xl mb-8">EXPERIENCE</h1>
-      <div>
-        <div className="job-header flex flex-col justify-between items-center">
-          {EXPERIENCE.map((e, index) => {
-            return (
-              <article key={index}>
-                <ListItem experience={e} />
-                <Spacer height={4} />
-              </article>
-            )
-          })}
-        </div>
-        <h3 className='text-lg italic text-end'>
-          <a href="/resume.pdf" target="_blank" rel="noopener noreferrer" className='link-hover'>
-            View full resume <FaExternalLinkAlt className="inline-block ml-2" />
-          </a>
-        </h3>
+      <div className="flex flex-col mb-6 gap-4">
+        {EXPERIENCE.map((e, index) => <ListItem key={index} experience={e} />)}
       </div>
+      <h3 className='text-lg italic text-end'>
+        <a href="/resume.pdf" target="_blank" rel="noopener noreferrer" className='link-hover text-xl'>
+          View full resume <FaExternalLinkAlt className="inline-block ml-2" />
+        </a>
+      </h3>
     </section>
   )
 }
--- src/app/components/projects.tsx
+++ src/app/components/projects.tsx
@@ -11,15 +11,15 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
   const { name, url, description, image, tags = [] } = project;
   return (
     <a href={url} target="_blank" rel="noopener noreferrer" className="group block mb-4">
-      <div className="flex flex-row items-start rounded-lg overflow-hidden border border-transparent transition duration-300 ease-in-out hover:bg-gray-800 hover:border-gray-700">
-        <div className="w-1/4 p-4 mt-3">
+      <div className="group flex flex-row items-start rounded-lg overflow-hidden border border-transparent transition duration-300 ease-in-out hover:bg-gray-800 hover:border-gray-700">
+        <div className="w-1/4 p-4 mt-1">
           <Image src={image} alt={name} width={150} height={75} className="align-top" />
         </div>
         <div className="flex flex-col p-4 w-3/4">
-          <h3 className="text-white text-xl font-semibold mt-2">
+          <h3 className="text-xl font-bold mb-2 link-hover">
             {name}
           </h3>
-          <p className="text-gray-300 text-base mt-2 secondary-text">{description}</p>
+          <p className="secondary-text text-base mt-2 secondary-text">{description}</p>
           <div className="flex flex-wrap mt-4">
             {tags.map((tag, index) => (
               <span key={index} className="bg-teal-700 text-teal-200 rounded-full px-3 py-1 text-xs font-semibold mr-2 mb-2">
@@ -35,7 +35,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
 
 const Projects: React.FC = () => {
   return (
-    <section className="">
+    <section>
       <h1 className="text-xl mb-8">PROJECTS</h1>
       <div className="flex flex-col mb-6 gap-4">
         {PROJECTS.map((p, index) => <ProjectCard key={index} project={p} />)}
