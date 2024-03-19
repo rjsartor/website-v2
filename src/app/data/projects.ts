c66d0239b8d9f6575646c323cbf26952a7b5122c
RJ <randal.sartor@gmail.com>
2024-03-18 21:29:01 -0400

    fix: remove node from dependencies

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -3,14 +3,14 @@ export const PROJECTS = [{
   url: 'https://www.roadrate.app',
   description: "A fully functioning web and mobile social media platform for anonymously rating and reviewing drivers in the United States. Users can search license plates to view ratings and reviews, or login to leave reviews and claim their own plates. Built with a React/React-Native front end and Node/Express back end, using Auth0 for authentication, and deployed with Netlify and Heroku.",
   image: '/images/road-rate.png',
-  tags: ['React', 'React-Native', 'Node.js', 'Auth0', 'MongoDB', 'Netlify', 'Heroku']
+  tags: ['Typescript', 'React', 'React-Native', 'Node.js', 'Auth0', 'MongoDB', 'Netlify', 'Heroku']
 },
 {
   name: 'Support Ticket Demo',
   url: 'https://support-ticket-demo.vercel.app/',
   description: "A support ticket demo that allows users to post support tickets and admins to view, update, and respond to them. Focuses on server-side rendering with Next.js.",
   image: '/images/support-ticket-demo.png',
-  tags: ['Next.js', 'Tailwind CSS', 'Postgres', 'Vercel']
+  tags: ['Typescript', 'Next.js', 'Tailwind CSS', 'Postgres', 'Vercel']
 },
 {
   name: 'MealCraft',
