1798d028dbf17a73b5d34a471d9137015fb6ac21
RJ <randal.sartor@gmail.com>
2024-03-27 11:01:42 -0400

    fix: deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,7 @@ export default function Home() {
     <div className='flex flex-col md:flex-row sm:flex-row justify-evenly mt-24'>
       <div className='layout-width'>
         <LeftPane />
-        <Spacer />
-        {/* <div className="flex-1"> */}
-          <RightPane />
-        {/* </div> */}
+        <RightPane />
       </div>
     </div>
   )
