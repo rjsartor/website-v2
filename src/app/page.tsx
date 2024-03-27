a7c8809d0c2f34da24c9618b59eb451ce696b23a
RJ <randal.sartor@gmail.com>
2024-03-27 11:38:08 -0400

    feat: adds basic mixpanel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+'use client';
 import LeftPane from './components/left-pane'
 import RightPane from './components/right-pane'
 import './globals.css'
+import { useEffect } from 'react';
+import { initializeMixpanel, trackEvent } from './services/mixpanel';
 
 export default function Home() {
+
+  useEffect(() => {
+    initializeMixpanel();
+    trackEvent('Page Viewed', { page: 'Home'});
+  }, []);
+
   return (
     <div className='flex flex-col md:flex-row sm:flex-row justify-evenly mt-24'>
       <div className='layout-width'>
---
+++ src/app/services/mixpanel.ts
@@ -0,0 +1,24 @@
+import mixpanel from 'mixpanel-browser';
+
+const MIXPANEL_TOKEN = process.env.NEXT_PUBLIC_MIXPANEL_TOKEN;
+
+const initializeMixpanel = (): void => {
+  if (typeof window !== 'undefined' && MIXPANEL_TOKEN) {
+    mixpanel.init(MIXPANEL_TOKEN, {debug: true, track_pageview: true, persistence: 'localStorage'});
+    console.log('mixpanel', mixpanel)
+  }
+};
+
+const trackEvent = (event: string, properties?: Record<string, unknown>): void => {
+    mixpanel.track(event, properties);
+};
+
+const trackPageView = (page: string): void => {
+  mixpanel.track('Page Viewed', { page });
+};
+
+const trackButtonClick = (buttonName: string): void => {
+  mixpanel.track('Button Clicked', { buttonName });
+};
+
+export { trackPageView, trackButtonClick, initializeMixpanel, trackEvent };
